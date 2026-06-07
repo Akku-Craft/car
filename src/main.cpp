d091d1d760f14e29a83c2ec1b83cd49f903dbaf7
AkkuCraft <infra@akku-craft.eu>
2026-06-07 17:38:08 +0200

    Test added
    Co-Authored-By: HenryMM <hello@henrymeyer.de>

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <U8g2lib.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// SH1106 1.3" OLED mit I2C (SDA, SCL)
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Wire.begin();
+  u8g2.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  u8g2.clearBuffer();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  u8g2.setFont(u8g2_font_ncenB14_tr);
+  u8g2.drawStr(0, 24, "Hallo OLED!");
+
+  u8g2.drawFrame(0, 32, 128, 32);
+  u8g2.drawCircle(64, 48, 10, U8G2_DRAW_ALL);
+  u8g2.drawStr(28, 56, "I2C SH1106");
+
+  u8g2.sendBuffer();
+  delay(1000);
 }
